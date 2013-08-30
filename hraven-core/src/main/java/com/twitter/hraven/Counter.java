4fdc4d463b4c0ab20019e11e2141591b386a97a9
Gary Helmling <ghelmling@twitter.com>
2013-08-30 10:11:58 -0700

    Counter objects should be immutable

--- hraven-core/src/main/java/com/twitter/hraven/Counter.java
+++ hraven-core/src/main/java/com/twitter/hraven/Counter.java
@@ -34,24 +34,12 @@ public class Counter {
     return group;
   }
 
-  public void setGroup(String group) {
-    this.group = group;
-  }
-
   public String getKey() {
     return key;
   }
 
-  public void setKey(String key) {
-    this.key = key;
-  }
-
   public long getValue() {
     return value;
-  }
-
-  public void setValue(long value) {
-    this.value = value;
   }
 
   @Override
